8d7db8a0073efa7f2345daa3a0d27d2a17e7464e
Анна Леоненко <AneedaLirena@yandex.ru>
2023-08-17 20:18:12 +0300

    final2.1

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -14,7 +14,7 @@ from foodgram import settings
 from recipes.models import Ingredient, Recipe, Tag
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.filters import  OrderingFilter, SearchFilter
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
